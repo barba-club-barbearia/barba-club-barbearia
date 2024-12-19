22d42191c6c4f1e4a0320f6a88510c2ed1fa1863
Marcus <barba.club.barbearia.2010@gmail.com>
2024-12-19 10:25:38 -0300

    chore: remocao console.log

--- src/components/IsAdmin/index.tsx
+++ src/components/IsAdmin/index.tsx
@@ -4,11 +4,7 @@ export default function IsAdmin() {
   if (typeof window === "undefined") return;
   const isAdmin = window.location.search.includes("admin");
 
-  console.log("isAdmin", isAdmin);
-
   const manifestPath = isAdmin ? "/manifest-admin.json" : "/manifest.json";
-
-  console.log("manifestPath", manifestPath);
 
   return <link rel="manifest" href={manifestPath} />;
 }
