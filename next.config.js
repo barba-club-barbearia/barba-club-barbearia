f124ca37781dbfa3b3b2fb2c3de0bba51313431f
Marcus <barba.club.barbearia.2010@gmail.com>
2025-01-07 21:30:19 -0300

    feat: removido o runtime do next-pwa

--- next.config.js
+++ next.config.js
@@ -5,31 +5,6 @@ const withPWA = require("next-pwa")({
   skipWaiting: true,
   disable: false,
   buildExcludes: [/middleware-manifest\.json$/], // Exclua arquivos problemáticos
-  runtimeCaching: [
-    {
-      urlPattern: /.*\.(png|jpg|jpeg|svg|webp)$/i, // Imagens
-      handler: "CacheFirst",
-      options: {
-        cacheName: "images",
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 dias
-        },
-      },
-    },
-    {
-      urlPattern: /.*\.(css|js)$/i, // CSS e JS
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-resources",
-        expiration: {
-          maxEntries: 50,
-        },
-      },
-    },
-  ],
-
-
 });
 
 
