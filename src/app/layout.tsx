a8765a1903aa59d98694256f4f0bad6cd95ced0e
Marcus <barba.club.barbearia.2010@gmail.com>
2025-01-08 19:14:56 -0300

    feat: adicao do manfifest no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,13 @@ import { Work_Sans } from "next/font/google";
 import "./globals.css";
 import { Suspense } from "react";
 import { SessionProviderComponent } from "@/components/SessionProvider";
+import RegisterSW from "@/components/ServiceWorker";
 const workSans = Work_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Barba Club",
   description: "Fila para cortar o Cabelo no Barba Club.",
+  manifest: "/manifest.json",
   icons: {
     icon: "/icon.png",
   },
@@ -21,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <Suspense>
+        <RegisterSW />
         <body className={`${workSans.className} antialiased`}>
           <SessionProviderComponent>{children}</SessionProviderComponent>
         </body>
