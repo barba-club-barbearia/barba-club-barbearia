70370e832fdbf16e974cc04cba262e10876c3f30
Marcus <barba.club.barbearia.2010@gmail.com>
2025-01-10 18:09:02 -0300

    fix: ajuste do worker e instalacao do babel

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const withPWA = require("next-pwa")({
   dest: "public",  // Garante que o Service Worker esteja na pasta 'public'
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',  // Certifique-se de que o PWA não está desabilitado
-  customWorkerDir: "worker", // Diretório onde está o custom Service Worker
+  disable: process.env.NODE_ENV === 'development',
 });
 
 
