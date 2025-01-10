894363d2ca7e7c756332b9a39697abc384ff90a6
Marcus <barba.club.barbearia.2010@gmail.com>
2025-01-10 09:22:38 -0300

    feat: retorno do next config como era

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const withPWA = require("next-pwa")({
   dest: "public",  // Garante que o Service Worker esteja na pasta 'public'
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  customWorkerSrc: "./custom-sw.js", // Aponta para o arquivo personalizado // Certifique-se de que o PWA não está desabilitado
+  disable: process.env.NODE_ENV === 'development',  // Certifique-se de que o PWA não está desabilitado
 });
 
 
