eb52ab6357b487c431513871fc40e264c9c2df68
Marcus <barba.club.barbearia.2010@gmail.com>
2025-01-20 18:10:40 -0300

    fix: removido arquivo nao utilizado

--- src/app/api/web-push/send/route.ts
+++
@@ -1,8 +0,0 @@
-import { sendNotification } from "@/contexts/Notification/NotificationSender";
-import { NextRequest } from "next/server";
-
-export async function POST(req: NextRequest) {
-  const { subscription, title, message } = await req.json();
-  await sendNotification(subscription, title, message);
-  return new Response(JSON.stringify({ message: "Push sent." }), {});
-}
